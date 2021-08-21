99368ac6bfcb4ca7d3b8d48a7f95eb7ee83cb144
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-08-20 21:07:50 -0700

    City Search displays info, but need to fix displaying past searches and 5 day forecast

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Weather-Report
+
+The goal for this assignment was to produce a web page where a user can search for a city and recieve a five day forecast; city name displaying the date, temperature, humidity, wind speed, and UV Index; and a list of previously searched cities. 
+
+![PNG of Weather Forecast]()
+
+--------------------------------------
+
+## The Process 
+
+I started of with a simple HTML set up that would display the needed text and boxes for the functions using bootstrap. After most of JS was done I went back and structured the site so it is easier to navigate. 
+
+Then I started working on getting the API key to work. I went over previous class examples and read over the suggested resources provided to get a better understanding of how to make a fetch request to provide the information needed. After making a functioning search button I noticed in the developer tools that it would fetch the required info so then I started working on getting it to display on the page. 
+
+Once the user searched for a city I was able to render all the searched cities, but realized that I needed to add an option to clear the results because the searched list was capped at 10 items.
+
+
+### Navigate my page
+
+Here is a link to my deployed page that will display a weather forecasting site that provides ////
+
+[Weather Forecast Link]()
---
+++ assets/js/index.js
@@ -0,0 +1,150 @@
+let weatherAPIKey = '27cf6ea7d803e00225bc37d5ded1aa6a';
+
+let currentCity;
+let priorCity;
+
+let weatherCondition = (event) => {
+  let city = $('#city-Search').val();
+  currentCity = $('#city-Search').val();
+
+  let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + weatherAPIKey;
+
+  fetch(queryURL)
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => {
+    console.log(response)
+    storeCity(city)
+
+    let iconImg = response.weather[0].icon;
+    let icon = "https://openweathermap.org/img/w/" + iconImg + ".png";
+
+    let momentTimeUTC = response.dt;
+    let momentTZOffset = response.timezone;
+    let momentTZOHours = momentTZOffset / 60 / 60;
+    let momentNow = moment.unix(momentTimeUTC).utc().utcOffset(momentTZOHours);
+    listCities();
+    fiveDayForecast(event);
+
+    $('#city-Name').text(response.name);
+
+    let weatherHTML = `<h3> ${response.name} ${momentNow.format("(MM/DD/YY)")}<img src="${icon}"></h3> <ul class="list-unstyled"> <li>Temp: ${response.main.temp}</li> <li>Wind: ${response.wind.speed}</li> <li> Humidity: ${response.main.humidity} </li> <li> UV Index: </li> </ul>`;
+
+    $('#city-Display').html(weatherHTML);
+  })
+}
+
+// Save the city to local storage
+let storeCity = (searchedCity) => {
+  let cityReal = false;
+
+  for (let i = 0; i < localStorage.length; i++) {
+    if (localStorage['cities' + i ] === searchedCity) {
+      cityReal = true;
+      break
+    }
+    if (cityReal === false ) {
+      localStorage.setItem('cities' + localStorage.length, searchedCity)
+    }
+  }
+}
+
+let fiveDayForecast = () => {
+  let city = $('#city-Search').val();
+  
+  let queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + weatherAPIKey;
+
+  fetch(queryURL)
+    .then((response) => {
+      return response.json()
+    })
+    .then((response) => {
+      //HTML formatting
+      let forecastHTML = ` <h2> 5-Day Forecast: </h2> <div class="flex-wrap d-inline-flex" id="five-day-display">`;
+
+      //use of UTC and Open Weather Map to display a 5 day forecast
+      let timeZoneOffset = response.city;
+      let timeZoneHours = timeZoneOffset;
+      let dayForecast = response.list[i];
+      let dayUTC = dayForecast.dt;
+      let currentMoment = moment.unix(dayUTC).utc().utcOffset(timeZoneHours);
+
+      let icon = "https://openweathermap.org/img/w/" + dayForecast.weather[0].icon + ".png";
+
+      if(currentMoment.format("HH:mm:ss") <= "23:00:00") {
+        forecastHTML += `<div class="card weather-boxes m-3> <ul class="list-unstyled" <li>${currentMoment.format("MM/DD/YY")}</li> <li class="weather-icon"><img src="${icon}"></li> <li>Temp: ${dayForecast.main.temp};</li> <li> Wind: ${dayForecast.wind.speed}</li> <li> Humidity: ${dayForecast.main.humidity}</li> </ul> </div>`;
+      }
+
+      forecastHTML += `</div>`;
+      $('#five-Days').html(forecastHTML);
+    })
+}
+
+let listCities = () => {
+  $('#city-Return').empty();
+  
+  // checks local storage and attributes a city value
+  if (localStorage.length === 0) {
+    if (priorCity) {
+      $('#city-Search').attr('value', priorCity);
+    } else {
+      $('#city-Search').attr('value', '');
+    }
+  } else {
+    let priorCityStored = 'cities' + (localStorage.length - 1);
+    priorCity = localStorage.getItem(priorCityStored)
+
+    $('#city-Search').attr('value', priorCity)
+
+    // display cities searched on the page
+    for (let i = 0; i < localStorage.length; i++) {
+      let city = localStorage.getItem('cities' + i)
+      let cityEl;
+
+      if (currentCity === '') {
+        currentCity === priorCity;
+      }
+      if (city === currentCity) {
+        cityEl = `<button type="button" class="list-group-item list-group-item-action active"> ${city}</button></li>`;
+      } else {
+        cityEl = `<button type="button" class="list-group-item list-group-item-action"> ${city}</button></li>`;
+      }
+      // displays all searched cities 
+      $('#city-Return').prepend(cityEl)
+    }
+
+    // option to clear searched cities
+     if (localStorage.length > 0) {
+         $('#search-Delete').html($('<a  id="search-Delete" href="#">Reset</a>'));
+       } else {
+         $('#search-Delete').html('');
+       }
+    }
+  }
+
+$('#search-Btn').on('click', (event) => {
+  event.preventDefault();
+  currentCity = $('#city-Search').val();
+  weatherCondition(event);
+}) 
+
+$('#city-Return').on('click', (event) => {
+  event.preventDefault(); 
+  $('#city-Search').val(event.target.textContent)
+  currentCity = $('#city-Search').val();
+  weatherCondition(event);
+})
+
+$('#search-Delete').on('click', () => {
+  localStorage.clear();
+  listCities();
+})
+
+listCities();
+
+weatherCondition();
+
+
+
+
